98321cbcccd0a2a72c6d7485601532d41098196f
Edo <edoardo.balsamo@sbw-media.ch>
2024-04-26 08:08:00 +0200

    webpage-finished-missin-vid

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const icons = document.querySelectorAll('.icon');
 
-    icons.forEach(function(icon) {
-        icon.addEventListener('click', function() {
+    icons.forEach(function (icon) {
+        icon.addEventListener('click', function () {
             this.classList.toggle('rotated');
             const contentId = this.id.replace('icon', 'content');
             const content = document.getElementById(contentId);
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (content.style.display === 'flex') {
                 content.style.display = 'none';
             } else {
-                document.querySelectorAll('.content').forEach(function(c) {
+                document.querySelectorAll('.content').forEach(function (c) {
                     c.style.display = 'none';
                 });
                 content.style.display = 'flex';
@@ -19,15 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.querySelector('a[href^="#"]').addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href'); 
-    const targetElement = document.querySelector(targetId); 
-    if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-});
 
+window.onscroll = function() {changeColorOnScroll()};
+
+function changeColorOnScroll() {
+    var element = document.getElementById("changeColor");
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) { // Change 100 to the scroll position where you want the color to change
+        element.classList.add("scrolled");
+    } else {
+        element.classList.remove("scrolled");
+    }
+}
 
 function scrollToTop() {
     // Scroll to the top of the page smoothly
